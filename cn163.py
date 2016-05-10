0e7d57e2e91fc5e53a355d7eb3bd63519838563d
Jiezhi.G <Jiezhi@users.noreply.github.com>
2016-05-10 15:14:55 +0800

    Create cn163.py
    Just a simple spider for cn163

---
+++ cn163.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*- 
+"""
+Created on 5/8/16
+
+@author: 'Jiezhi.G@gmail.com'
+
+Reference: 
+"""
+import requests
+from bs4 import BeautifulSoup
+import csv
+import time
+import os.path
+
+
+def get_content_from_url(url):
+    split_url = url.split('/')
+    number = split_url[len(split_url) - 2]
+    print(url, number)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'lxml')
+    title = soup.title.get_text().split('|')[0].strip()
+    file_title = os.path.join('result', number + title.split('/')[0].strip() + '.csv')
+    print('parsing %s' % title)
+
+    # if the result file exists then continue
+    if os.path.exists(file_title):
+        print('%s already parsed' % file_title)
+    else:
+        with open(file_title, 'w') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            for link in soup.find('div', {'id': 'entry'}).findAll('a'):
+                item_name = link.get_text().strip()
+                item_value = link.get('href').strip()
+                # exclude irrelevant url
+                if item_value.startswith('http://cn163.net'):
+                    continue
+                csvwriter.writerow([item_name, item_value])
+    try:
+        next_url = soup.find('a', {'rel': 'next'}).get('href').strip()
+    except AttributeError:
+        print('it seems no more url to get')
+        exit(0)
+    time.sleep(5)
+    get_content_from_url(next_url)
+
+
+if __name__ == '__main__':
+    first_url = 'http://cn163.net/archives/58/'
+    get_content_from_url(first_url)
